9f054ea9191725a948c477f8de8d696b71e676bf
fnfly2005 <fnfly2005@aliyun.com>
2017-09-11 21:27:48 +0800

    2017-09-11

---
+++ 05tmp/djk05.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+t1=${1:-`date -d "yesterday ${clock}" +"%Y-%m-%d %T"`}
+t2=${2:-`date -d "${t1% *} 1days ${clock}" +"%Y-%m-%d %T"`}
+path="/data/fannian/"
+fun() {
+echo `cat ${path}sql/${1}.sql | sed "s/-time1/${2:-${t1% *}}/g;
+s/-time2/${3:-${t2% *}}/g;s/-time3/${4:-${t3% *}}/g"`
+}
+ag=`fun appcontentclicklog_gz` 
+dmsg=`fun djk_m_sourceubtpageview_gz`
+
+file="djk05"
+attach="${path}00output/${file}.csv"
+presto_e="/opt/presto/bin/presto --server hc:9980 --catalog hive --execute "
+se="set session optimize_hash_generation=true;"
+
+${presto_e}"
+${se}
+with dmsg as (
+		${dmsg}
+		and trackercode like 'djk_js%'
+		),
+temp as (select 1)
+	select
+		count(1)
+	from
+		dmsg
+"|grep -iv "SET"
+#>${attach}
