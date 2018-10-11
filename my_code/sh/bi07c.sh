5e5810f0778cce96d51b046b7d942797290af36c
fnfly2005 <fnfly2005@aliyun.com>
2018-10-11 21:16:42 +0800

    上线埋点业务参数复验工具

--- my_code/sh/bi07c.sh
+++ my_code/sh/bi07c.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 source ./my_code/fuc.sh
-fpw=`fun detail_myshow_pv_wide_report.sql ut`
-wpv=`fun my_code/sql/dim_myshow_pv.sql u`
+fmw=`fun detail_myshow_mv_wide_report.sql ut`
+wmv=`fun my_code/sql/dim_myshow_mv.sql u`
 mke=`fun my_code/sql/myshow_unused_key.sql`
 
 fus() {
     echo "
     select
-        pv_id,
+        mv_id,
         cp.page_identifier,
-        case when biz_par is null then ck
-        when length(biz_par)<2 then ck
-        else (biz_par || '&' || ck) end as ck
+        cp.event_id,
+        case when \$biz_par is null then ck
+        when length(\$biz_par)<2 then ck
+        else (\$biz_par || '&' || ck) end as ck
     from (
         select
             fp.page_identifier,
+            fp.event_id,
             array_join(array_agg(fp.ck),'&','null') ck
         from (
             select distinct
                 page_identifier,
+                event_id,
                 ck
             from (
                 select
                     page_identifier,
-                    map_keys(custom) as cus_key
-                $fpw
-                    and partition_biz_bg<2
-                    and map_keys(custom) is not null
-                    and map_keys(custom)<>array[]
+                    event_id,
+                    map_keys(\$custom) as cus_key
+                $fmw
+                    and map_keys(\$custom) is not null
+                    and map_keys(\$custom)<>array[]
                 group by
-                    1,2
-                ) fpw
+                    1,2,3
+                ) fmv
                 CROSS JOIN UNNEST(cus_key) as t (ck)
             where
                 ck is not null
@@ -42,33 +45,39 @@ fus() {
             left join (
                 select distinct
                     page_identifier,
+                    event_id,
                     ck
                 from (
                     select
                         page_identifier,
-                        split(biz_par,'&') cus_key
-                    $wpv
-                        and biz_par is not null
-                    ) wpv
+                        event_id,
+                        split(\$biz_par,'&') cus_key
+                    $wmv
+                        and \$biz_par is not null
+                     ) wmv
                     CROSS JOIN UNNEST(cus_key) as t (ck)
-                where ck is not null
+                where 
+                    ck is not null
                 ) wp
             on wp.page_identifier=fp.page_identifier
+            and wp.event_id=fp.event_id
             and wp.ck=fp.ck
         where
             wp.ck is null 
             and mke.unused_key is null
         group by
-            1
+            1,2
         ) as cp
         left join (
             select
-                pv_id,
+                mv_id,
                 page_identifier,
-                biz_par
-            $wpv
-            ) pv
-        on pv.page_identifier=cp.page_identifier
+                event_id,
+                \$biz_par
+            $wmv
+            ) mv
+        on mv.page_identifier=cp.page_identifier
+        and mv.event_id=cp.event_id
     ${lim-;}"
 }
 
