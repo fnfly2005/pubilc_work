0208a83800b2bdcd8512bb563695ee629b17f063
fnfly2005 <fnfly2005@aliyun.com>
2018-10-12 20:49:32 +0800

    例行更新

---
+++ my_code/sql/myshow_parkeylist.sql
@@ -0,0 +1,7 @@
+/*非演出流量业务参数键值*/
+select
+    par_key,
+    is_myshow,
+    biz_tag
+from 
+    upload_table.myshow_parkeylist
--- my_code/sql/myshow_unused_key.sql
+++
@@ -1,5 +0,0 @@
-/*非演出流量业务参数键值*/
-select
-    unused_key
-from 
-    upload_table.myshow_unused_key
