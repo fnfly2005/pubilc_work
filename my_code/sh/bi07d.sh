045436d2ab7dfab72d7fd7abf11d73e2bbdfcac4
fnfly2005 <fnfly2005@aliyun.com>
2018-10-12 20:50:12 +0800

    上线埋点业务参数复验工具(支持新增业务参数监控、支持快速验证已入库埋点业务参数-效率约提高10-20倍)

---
+++ my_code/sh/bi07d.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#埋点键值核验工具
+source ./my_code/fuc.sh
+fmw=`fun detail_myshow_mv_wide_report.sql ut`
+fpw=`fun detail_myshow_pv_wide_report.sql ut`
+wmv=`fun my_code/sql/dim_myshow_mv.sql u`
+mke=`fun my_code/sql/myshow_parkeylist.sql`
+
+fus() {
+    echo "
+        select distinct
+            fp.ck,
+            is_myshow,
+            biz_tag,
+            case when mke.par_key is null then 1
+            else 0 end as is_new
+        from (
+            select distinct
+                ck
+            from (
+                select
+                    map_keys(custom) as cus_key
+                $fmw
+                    and map_keys(custom) is not null
+                    and map_keys(custom)<>array[]
+                group by
+                    1
+                ) fmv
+                CROSS JOIN UNNEST(cus_key) as t (ck)
+            where
+                ck is not null
+            union all
+            select distinct
+                ck
+            from (
+                select
+                    map_keys(event_attribute) as cus_key
+                $fmw
+                    and map_keys(event_attribute) is not null
+                    and map_keys(event_attribute)<>array[]
+                group by
+                    1
+                ) fmv
+                CROSS JOIN UNNEST(cus_key) as t (ck)
+            where
+                ck is not null
+            union all
+            select distinct
+                ck
+            from (
+                select
+                    map_keys(custom) as cus_key
+                $fpw
+                    and partition_biz_bg<2
+                    and map_keys(custom) is not null
+                    and map_keys(custom)<>array[]
+                group by
+                    1
+                ) fpw
+                CROSS JOIN UNNEST(cus_key) as t (ck)
+            where
+                ck is not null
+            ) fp
+            left join (
+                $mke
+                ) mke
+            on mke.par_key=fp.ck
+    ${lim-;}"
+}
+
+downloadsql_file $0
+fuc $1
