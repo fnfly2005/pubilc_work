1bc14898b2ac27a67f733887f532478bd58534a8
fnfly2005 <fnfly2005@aliyun.com>
2018-10-15 13:09:41 +0800

    调整基础数据源，提升准确性

--- my_code/sh/bd24.sh
+++ my_code/sh/bd24.sh
@@ -1,36 +1,16 @@
 #!/bin/bash
-#--------------------猫眼演出readme-------------------
-#*************************项目销售-全量*******************
-# 优化输出方式,优化函数处理
-path="/Users/fannian/Documents/my_code/"
-fun() {
-    if [ $2x == dx ];then
-        echo `cat ${path}sql/${1} | grep -iv "/\*" | sed '/where/,$'d`
-    elif [ $2x == ux ];then
-        echo `cat ${path}sql/${1} | grep -iv "/\*" | sed '1,/from/'d | sed '1s/^/from/'`
-    elif [ $2x == tx ];then
-        echo `cat ${path}sql/${1} | grep -iv "/\*" | sed "s/begindate/today{-1d}/g;s/enddate/today{-0d}/g"`
-    elif [ $2x == utx ];then
-        echo `cat ${path}sql/${1} | grep -iv "/\*" | sed "s/begindate/today{-1d}/g;s/enddate/today{-0d}/g" | sed '1,/from/'d | sed '1s/^/from/'`
-    else
-        echo `cat ${path}sql/${1} | grep -iv "/\*"`
-    fi
-}
+#项目销售-全量-多维度-多指标
+source ${CODE_HOME-./}my_code/fuc.sh
 
-spo=`fun detail_myshow_salepayorder.sql`
+spo=`fun detail_myshow_saleorder.sql`
 dmp=`fun dim_myshow_performance.sql`
 dc=`fun dim_myshow_customer.sql`
-dpr=`fun dim_myshow_project.sql`
-md=`fun myshow_dictionary.sql`
+md=`fun dim_myshow_dictionary.sql`
 
-file="bd24"
-lim=";"
-attach="${path}doc/${file}.sql"
-
+fus() {
 echo "
 select
-    case when 0 in (\$dim) then 
-        substr(dt,1,7) 
+    case when 0 in (\$dim) then mt
     else 'all' end as mt,
     case when 1 in (\$dim) then dt
     else 'all' end as dt,
@@ -44,65 +24,38 @@ select
     else 'all' end as customer_name,
     area_1_level_name,
     area_2_level_name,
-    province_name,
-    city_name,
-    category_name,
+    dmp.province_name,
+    dmp.city_name,
+    dmp.category_name,
     spo.performance_id,
-    performance_name,
-    shop_name,
-    case when 3 not in (\$dim) and 4 not in (\$dim) then 'all'
-        when dpr.bd_name is null then '无'
-    else dpr.bd_name end as bd_name,
-    sum(order_num) as order_num,
+    dmp.performance_name,
+    dmp.shop_name,
+    bd_name,
+    count(distinct order_id) as order_num,
     sum(ticket_num) as ticket_num,
     sum(TotalPrice) as TotalPrice,
     sum(grossprofit) as grossprofit
 from (
-    select
-        partition_date as dt,
-        sellchannel,
-        customer_id,
-        project_id,
-        performance_id,
-        count(distinct order_id) as order_num,
-        sum(salesplan_count*setnumber) as ticket_num,
-        sum(TotalPrice) as TotalPrice,
-        sum(grossprofit) as grossprofit
-    from 
-        mart_movie.detail_myshow_salepayorder
-    where 
-        partition_date>='\$\$begindate'
-        and partition_date<'\$\$enddate'
-        and category_id in (\$category_id)
-    group by
-        1,2,3,4,5
+    $spo
     ) spo
-    left join (
-    $dmp
-    ) dmp
+    join (
+        $dmp
+            and category_id in (\$category_id)
+        ) dmp
     using(performance_id)
     left join (
     $dc
     ) dc
     on spo.customer_id=dc.customer_id
     left join (
-    $dpr
-    ) dpr
-    on dpr.project_id=spo.project_id
-    left join (
-    $md
-    and key_name='sellchannel'
+        $md
+        and key_name='sellchannel'
     ) md
     on md.key=spo.sellchannel
 group by
     1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
-$lim">${attach}
+${lim-;}"
+}
 
-echo "succuess!"
-echo ${attach}
-if [ ${1}r == pr ]
-#加上任意字符，如r 避免空值报错
-then
-cat ${attach}
-#命令行参数为p时，打印输出文件
-fi
+downloadsql_file $0
+fuc $1
