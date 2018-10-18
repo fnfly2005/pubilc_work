0467527487869afc2add3969e4bd9a767adca8d5
fnfly2005 <fnfly2005@aliyun.com>
2018-10-18 23:44:25 +0800

    按DRY原则优化、新增etl模式

--- my_code/mou.sh
+++ my_code/mou.sh
@@ -4,16 +4,25 @@
 path="${CODE_HOME}my_code/"
 file="${path}sh/${2}.sh"
 
+cat_api() {
+cat ${path}api.sh | head -$1 | tail -$2 >$file
+}
+
+if [ ${1}r == hr ]
+then
+echo "参数1为模式，n为常规模式，r为实时模式，e为etl模式。参数2为创建文件"
+exit 0
+fi
+
 if [ ${1}r == nr ]
 #加上任意字符，如r 避免空值报错
 then
-cat ${path}api.sh | head -16 | tail -13 >$file
-echo "succuess! mode:normal path:$file"
+`cat_api 16 13`
 elif [ ${1}r == rr ]
 then 
-cat ${path}api.sh | head -33 | tail -16 >$file
-echo "succuess! mode:realtime path:$file"
-elif [ ${1}r == hr ]
+`cat_api 33 16`
+elif [ ${1}r == er ] 
 then
-echo "参数1为模式，n为常规模式，r为实时模式，参数2为创建文件"
+`cat_api 45 11`
 fi
+echo "succuess! mode:normal path:$file"
