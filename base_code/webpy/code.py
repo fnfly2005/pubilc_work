1d1bc83122907cc5bf56323bac112420c2f73032
fnfly2005 <fnfly2005@aliyun.com>
2018-10-20 22:56:22 +0800

    dbwebpytest

--- base_code/webpy/code.py
+++ base_code/webpy/code.py
@@ -10,14 +10,16 @@ Version: v1.0
 ##################################
 import web
 
+db = web.database(dbn='mysql',user='fannian',pw='I$p4xuzRl0fUJ6JZ',db='webpy')
 render = web.template.render('templates/')
 
 urls = (
-    '/(.*)','index'
+    '/','index',
 )
 
 class index:
-    def GET(self,name):
+    def GET(self):
+        todos = db.select
         return render.index(name)
 
 if __name__ == "__main__":
