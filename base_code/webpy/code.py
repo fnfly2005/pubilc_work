46ccfb952a88b1a7f948616c51b6d1d1064fe995
fnfly2005 <fnfly2005@aliyun.com>
2018-10-21 13:29:25 +0800

    完成动态网站demo

--- base_code/webpy/code.py
+++ base_code/webpy/code.py
@@ -10,17 +10,24 @@ Version: v1.0
 ##################################
 import web
 
-db = web.database(dbn='mysql',user='fannian',pw='I$p4xuzRl0fUJ6JZ',db='webpy')
+db = web.database(dbn='mysql',port=3306,host='127.0.0.1',user='fannian',pw='1a9SFQKh8YtG',db='webpy')
 render = web.template.render('templates/')
 
 urls = (
     '/','index',
+    '/add','add'
 )
 
 class index:
     def GET(self):
-        todos = db.select
-        return render.index(name)
+        todos = db.select('todo')
+        return render.index(todos)
+
+class add:
+    def POST(self):
+        i = web.input()
+        n = db.insert('todo',title=i.title)
+        raise web.seeother('/')
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
