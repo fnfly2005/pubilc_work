99ae67d91f72cbace949dc88f10011d08df1240a
fnfly2005 <fnfly2005@aliyun.com>
2018-10-25 12:56:07 +0800

    解析出订单粒度-订单金额及日期

--- base_code/tborder/process_order.py
+++ base_code/tborder/process_order.py
@@ -13,16 +13,27 @@ from re import match
 dic1=['退款/退换货','交易成功','花呗账单','运费险已失效','买家已付款','仓库已发货','保险服务','订单详情','查看物流','确认收货','再次购买','申请开票','发货清单','申请售后','追加评论','双方已评']
 
 def workflow():
+    order_id=[]
+    order_list=[]
     i=1
     for line in sys.stdin:
         line_srp = line.strip()
         if len(line_srp) > 0 and line_srp not in dic1 \
             and match('\(含运费[\S]+|[\S]+\：[\S]+|还剩[\S]+',line_srp) == None:
-            if match('[\S]+订单号[\S]+',line_srp) == None:
-                print line_srp
+            if match('[\S]+订单号[\S]+',line_srp) == None \
+                or i == 1:
+                order_id.append(line_srp)
             else:
-                print str(i) + '\t' + line_srp
-                i=i+1
+                order_list.append(order_id)
+                order_id=[]
+                order_id.append(line_srp)
+            i=i+1
+    for oid in order_list:
+        totalprice=str(oid.pop(4))[3:]
+        orderdt=str(oid.pop(0))[0:10]
+        for i in oid:
+            tes = orderdt + '\t' + totalprice + '\t' + str(i)
+            print tes
 
 if __name__ == '__main__':
     workflow()
