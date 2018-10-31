b395eaf90f559b150bf16c2abbf24e8bd1cfa8b4
fnfly2005 <fnfly2005@aliyun.com>
2018-10-31 11:07:23 +0800

    python操作mysql调试

--- base_code/etl/file2table.py
+++ base_code/etl/file2table.py
@@ -8,3 +8,16 @@ Date:
 Version: v1.0
 '''
 ##################################
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(host='localhost',user='fnfly2005',passwd=sys.argv[1],db='upload_table',port=3306,charset='utf8')
+cursor = db.cursor()
+cursor.execute('drop table if exists sale_offline')
+sql= """create table sale_offline (
+
+   )"""
+
+cursor.execute(sql)
+
+db.close()
