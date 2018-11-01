5b010b56e435d66b1a4cdec97f9be9d7d43bf89e
fnfly2005 <fnfly2005@aliyun.com>
2018-11-01 11:59:43 +0800

    读取文件并打印字段和判断字段类型

--- base_code/etl/file2table.py
+++ base_code/etl/file2table.py
@@ -10,14 +10,32 @@ Version: v1.0
 ##################################
 import MySQLdb
 import sys
-
+import re
+'''
 db = MySQLdb.connect(host='localhost',user='fnfly2005',passwd=sys.argv[1],db='upload_table',port=3306,charset='utf8')
 cursor = db.cursor()
 cursor.execute('drop table if exists sale_offline')
 sql= """create table sale_offline (
 
-   )"""
+   ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT=''"""
 
 cursor.execute(sql)
 
 db.close()
+'''
+def isnumber(s):
+    try:
+        float(s)
+        if s.isdigit():
+            print str(s) + "int(11)"
+        else:
+            print str(s) + "decimal(10,2)"
+    except:
+        print s + "varchar(20)"
+        
+with open(sys.argv[1],'rb') as upfile:
+    upfile_list=upfile.readlines()
+    field_list=upfile_list[0].strip().split(',')
+    data_list=upfile_list[1].strip().split(',')
+    for d in data_list:
+        isnumber(d)
