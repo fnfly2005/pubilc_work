6bc9cb747d3b58cbc8615c90b0d63adeef736a44
fnfly2005 <fnfly2005@aliyun.com>
2018-11-05 14:38:02 +0800

    修复mysql建表字符集问题

--- base_code/etl/file2table.py
+++ base_code/etl/file2table.py
@@ -14,7 +14,7 @@ import re
 def ConMysql(sqlfiles,Tablename,mysqlpw):
     db = MySQLdb.connect(host='localhost',user='fnfly2005',passwd=mysqlpw,db='upload_table',port=3306,charset='utf8')
     cursor = db.cursor()
-    cursor.execute('drop table if exists ' + Tablename)
+    cursor.execute("DROP TABLE IF EXISTS " + Tablename)
     with open(sqlfiles) as sqlfile:
         sql = sqlfile.read()
     cursor.execute(sql)
@@ -31,7 +31,7 @@ def isnumber(s):
         return "varchar(20)"
 
 def CreTabSql(Files,Tablename):
-    print "create table Tablename ("
+    print "create table " + Tablename + " ("
     with open(Files,'rb') as upfile:
         upfile_list=upfile.readlines()
         field_list=upfile_list[0].strip().split(',')
@@ -42,18 +42,18 @@ def CreTabSql(Files,Tablename):
             if d < dl:
                 ed=","
             else:
-                ed=") ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT=''"
+                ed=") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT=''"
             print f + '\t' + isnumber(data_list[d]) + '\t' + "COMMENT ''" + ed
             d = d + 1
 
 desc="""当参数1=c时：根据输入文件生成建表语句 参数2为输入文件 参数3为表名
-当参数2=m时：根据输入SQL文件建表 参数2位输入文件 参数3为表名 参数4为mysql密码"""
-try:
-    if sys.argv[1] == 'c':
-        CreTabSql(sys.argv[2],sys.argv[3])
-    elif sys.argv[1] == 'm':
-        ConMysql(sys.argv[2],sys.argv[3],sys.argv[4])
-    else:
-        print desc
-except:
+当参数2=m时：根据输入SQL文件建表 参数2位输入文件 参数3为表名，参数4为mysql密码"""
+#try:
+if sys.argv[1] == 'c':
+    CreTabSql(sys.argv[2],sys.argv[3])
+elif sys.argv[1] == 'm':
+    ConMysql(sys.argv[2],sys.argv[3],sys.argv[4])
+else:
     print desc
+#except:
+    #print desc
