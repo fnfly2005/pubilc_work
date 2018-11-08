b1381ced6173b35c4e909567571606d7e4146992
fnfly2005 <fnfly2005@aliyun.com>
2018-11-08 12:14:39 +0800

    修正字符集问题

--- base_code/sh/sedgrep.sh
+++ base_code/sh/sedgrep.sh
@@ -3,6 +3,6 @@
 if [[ ${1}r == hr ]];then
     echo "批量替换工具 参数1为原字符 参数2为新字符 参数3为路径"
 else
-    sed -i '' 's/'"$1"'/'"$2"'/g' \
-    `grep $1 -rl ${3-my_code/}`
+    LC_CTYPE=C sed -i '' 's/'$1'/'$2'/g' \
+    `grep $1 -rl ${3}`
 fi
