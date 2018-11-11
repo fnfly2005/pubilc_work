429b4e06293188c972226cbb94e7f52c8a78a2be
fnfly2005 <fnfly2005@aliyun.com>
2018-11-11 20:30:49 +0800

    新增文件替换模式

--- base_code/sh/sedgrep.sh
+++ base_code/sh/sedgrep.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
 #批量替换工具
-if [[ ${1}r == hr ]];then
-    echo "批量替换工具 参数1为原字符 参数2为新字符 参数3为路径"
-else
-    LC_CTYPE=C sed -i '' 's/'$1'/'$2'/g' \
-    `grep $1 -rl ${3}`
-fi
+helpinfo="批量替换工具——参数1为模式选择,参数2为原字符,参数3为新字符
+当参数1=p时,参数4为路径
+当参数1=s时,参数4为文件"
+
+case $1 in
+    h) echo $helpinfo ;;
+    p) LC_CTYPE=C sed -i '' 's/'$2'/'$3'/g' `grep $2 -rl $4`;;
+    s) LC_CTYPE=C sed -i 's/'$2'/'$3'/g' $4;;
+esac
