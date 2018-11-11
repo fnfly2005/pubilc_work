65a1957a4ec3972703eb8b86d42f5f9e32646d4e
fnfly2005 <fnfly2005@aliyun.com>
2018-11-11 20:51:53 +0800

    修改路径

--- base_code/sh/excel2csv.sh
+++ base_code/sh/excel2csv.sh
@@ -3,7 +3,7 @@
 if [ $1 = h ];then
     echo "1:input_file 2:output_file 3:sheet_num[0,1,2,...]"
 else
-    tmp="$HOME/Documents/tmp.xlsx"
+    tmp="$HOME/tmp.xlsx"
     cp $1 $tmp
     python ${public_home}/base_code/py/excel2csv.py $tmp $2 $3
     rm $tmp
