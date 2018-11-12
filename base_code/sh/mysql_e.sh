401d7130795cac0ad855a3cab119360cff0b4161
fnfly2005 <fnfly2005@aliyun.com>
2018-11-12 16:36:57 +0800

    业务逻辑函数模块化

--- base_code/sh/mysql_e.sh
+++ base_code/sh/mysql_e.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-if [[ $1 = 'h' ]];then
-    echo "参数1为执行SQL文件,参数2为密码,参数3为库名"
+source $public_home/base_code/etl/etl_fuc.sh
+
+if [[ ${1}r == 'hr' ]];then
+    echo "参数1为执行SQL文件,参数2为密码"
 else
-    mysql -ufnfly2005 -D${3-upload_table} < $1 -p$2
+    mysqle $1 $2
 fi
