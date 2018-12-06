cc5ec560aa8043ed9975928d9cca4ff9e04d5310
fnfly2005 <fnfly2005@aliyun.com>
2018-12-06 10:32:12 +0800

    优化循环语句

--- base_code/demo/sort.py
+++ base_code/demo/sort.py
@@ -1,40 +1,44 @@
 #!/usr/bin/python
 #coding:utf-8
-ar=[12,443,32,5,-5]
+ar=[12,443,32,5,500]
 
 def swap(array,a1,a2):
-    #实现数组中两个值的位置互换
+    """实现数组中两个值的位置互换"""
     temp=array[a1]
     array[a1]=array[a2]
     array[a2]=temp
 
 def selectSort(arr):
-    #通过选择排序算法实现顺序排列
-    for a in range(0,len(arr)-1):
+    """通过选择排序算法实现顺序排列"""
+    for a in range(len(arr)-1):
         for x in range(a+1,len(arr)):
             if arr[a]>arr[x]:
                 swap(arr,a,x)
 
 def bubbleSort(arr):
-    #通过冒泡排序算法实现顺序排列
+    """通过冒泡排序算法实现顺序排列"""
     for a in range(1,len(arr)):
-        for x in range(0,len(arr)-a):
+        for x in range(len(arr)-a):
             if arr[x]>arr[x+1]:
                 swap(arr,x,x+1)
 
 def bubbleSortPro(arr):
-    #通过冒泡排序算法实现顺序排列,利用中间变量提高性能
-    for a in range(0,len(arr)-1):
+    """通过冒泡排序算法实现顺序排列,利用中间变量提高性能"""
+    for a in range(len(arr)-1):
         tmp = arr[0]
         tp = 0
         for x in range(1,len(arr)-a):
             if arr[x] > tmp:
                 tmp = arr[x]
                 tp = x
-            if x == len(arr)-a-1:
-                swap(arr,tp,x)
+        else:
+            swap(arr,tp,x)
+
+def rangeTest(a):
+    for r in range(a):
+        print r
 
 if __name__ == '__main__':
     print ar
-    bubbleSortPro(ar)
+    selectSort(ar)
     print ar
