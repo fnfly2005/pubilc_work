03651f38a431b763bc61c1fe5cebb9883e9370ed
fnfly2005 <fnfly2005@aliyun.com>
2018-12-09 17:23:15 +0800

    优化py

--- base_code/api.py
+++ base_code/api.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 #coding:utf-8
 ##################################
-'''
-Path: 
+"""
 Description: 
-Date: 
 Version: v1.0
-'''
+"""
 ##################################
 if __name__ == '__main__':
     pass
