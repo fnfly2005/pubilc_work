9c041389e85db701d21f2a178bb4f735ffb8b972
fnfly2005 <fnfly2005@aliyun.com>
2018-12-10 15:47:20 +0800

    变量学习

---
+++ learntoprogram/variable.java
@@ -0,0 +1,14 @@
+/*变量*/
+class variable
+{
+    public static void main(String[] args)
+    {
+        String a;
+        a = "Hello World!";
+        System.out.println(a);
+
+        int b = 6;
+        byte c = (byte) b;
+        System.out.println(c);
+    }
+}
