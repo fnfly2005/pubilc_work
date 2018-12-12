7da252e26a10f4d5b16cd4d72012f71a993052d3
fnfly2005 <fnfly2005@aliyun.com>
2018-12-12 11:25:11 +0800

    if 语句季节星期 java

--- learntoprogram/sentence.java
+++ learntoprogram/sentence.java
@@ -3,9 +3,37 @@ class sentence
 {
     public static void main(String[] args)
     {
-        if (true)
+        int x = 5;
+        seasonCon(x);
+    }
+    static void weekCon(int x)
+    /*if 语句判断星期*/
+    {   
+        char [] weekar = {'一','二','三','四','五','六','日'};
+        if (x > 0 && x < 8)
         {
-            System.out.println("Hello World!");
+            System.out.println("星期" + weekar[x-1]);
         }
+        else
+            System.out.println("星期" + x + "不存在");
+    }
+    static void seasonCon(int x)
+    /*if 语句判断季节*/
+    {
+        String y = "";
+        if (x > 0 && x < 13)
+        {
+            if (x == 12 || x <= 2)
+                y = "冬";
+            else if (x >= 3 && x <=5)
+                y = "春";
+            else if (x >= 6 && x <=8)
+                y = "夏";
+            else if (x >= 9 && x <=11)
+                y = "秋";
+            System.out.println(x + "月是" + y + "季");
+        }
+        else
+            System.out.println(x + "月不存在");
     }
 }
