13bdef0d649feec1d065c8c512135ae83fd06de3
fnfly2005 <fnfly2005@aliyun.com>
2018-12-22 23:19:37 +0800

    优化异常处理

--- base_code/py/excel2csv.py
+++ base_code/py/excel2csv.py
@@ -11,17 +11,15 @@ Version: v2.0
 import sys
 import pandas as pd
 
+in_file = sys.argv[1]
+out_file = sys.argv[2]
 try:
-    in_file = sys.argv[1]
-    out_file = sys.argv[2]
-    try:
-        index = int(sys.argv[3])
-    except:
-        index = 0
-    def excel2csv():
-        data_xls = pd.read_excel(in_file, sheet_name = index,index_col = 0)
-        data_xls.to_csv(out_file, encoding = 'utf-8')
-    if __name__ == '__main__':
-        excel2csv()
+    index = int(sys.argv[3])
 except:
-    print "输入参数有误"
+    index = 0
+
+def excel2csv():
+    data_xls = pd.read_excel(in_file, sheet_name = index,index_col = 0)
+    data_xls.to_csv(out_file, encoding = 'utf-8')
+if __name__ == '__main__':
+    excel2csv()
