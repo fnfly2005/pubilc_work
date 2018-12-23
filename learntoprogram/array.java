a8e5265e5d2e3d3cf8e2ff0e0f05cd8317c66d99
fnfly2005 <fnfly2005@aliyun.com>
2018-12-23 18:21:08 +0800

    数组运算-java

--- learntoprogram/array.java
+++ learntoprogram/array.java
@@ -35,6 +35,77 @@ class array
         }
     }
 
+    static void bubbleSort(int[] arr)
+    {//通过冒泡排序算法实现顺序排列
+        for (int x=1;x<arr.length-1;x++)
+        {
+            for (int y=0;y<arr.length-x;y++)
+            {
+                if (arr[y]>arr[y+1])
+                {
+                    swap(arr,y,y+1);
+                }
+            }
+        }
+    }
+
+    static void bubbleSortPro(int[] arr)
+    {//通过冒泡排序算法实现顺序排列,利用中间变量提高性能
+        for (int x=1;x<arr.length;x++)
+        {
+            int tmp = 0;
+            for (int y=0;y<arr.length-x;y++)
+            {
+                if (arr[tmp]<arr[y+1])
+                {
+                    tmp = y+1;
+                }
+            }
+            if (tmp == arr.length-x)
+            {
+                continue;
+            }
+            else
+            {
+                swap(arr,tmp,arr.length-x);
+            }
+        }
+    }
+
+    static int search(int[] arr,int v)
+    {//在指定数组中循环查找给定值,并返回值所在角标
+        for (int x=0;x<arr.length;x++)
+        {
+            if(arr[x] == v)
+            {
+                return x;
+            }
+        }
+        return -1;
+    }
+
+    static int binarySearch(int[] arr,int v)
+    {//在指定数组中通过折半查找算法查找给定值,并返回值所在角标
+        int min = 0,max = arr.length-1,mid;
+        while (min <= max)
+        {    
+            mid = (min + max)/2;
+            if (arr[mid] == v)
+            {    
+                return mid;
+            }
+            else if (arr[mid] > v)
+            {
+                max = mid-1;
+            }
+            else
+            {
+                min = mid+1;
+            }
+        }
+        return -1;
+    }
+
     static void loopArray(int[] arr)
     {//遍历并打印数组
         System.out.print('[');
@@ -53,7 +124,8 @@ class array
     {
         int [] ar = {12,443,32,5,500,27,200,64};
         loopArray(ar);
-        selectSort(ar);
+        bubbleSortPro(ar);
         loopArray(ar);
+        System.out.println(binarySearch(ar,200));
     }
 }
