97b25f85ae8284f390283b4bfe8a4b02bb188067
fnfly2005 <fnfly2005@aliyun.com>
2018-12-25 10:41:49 +0800

    封装成员变量

--- learntoprogram/array.py
+++ learntoprogram/array.py
@@ -11,22 +11,30 @@ ar = [12,443,32,5,500,27,200,64]
 class array:
     "用于处理数组对象"
     def __init__(self,arr=[]):
-        """构造函数，用于初始化数组对象"""
-        self.data = arr
+        """构造函数，用于初始化数组对象,并封装变量"""
+        self.__data = arr
+
+    def setData(self,arr):
+        """修改数据值"""
+        self.__data = arr
+
+    def getData(self):
+        """获取数据值"""
+        return self.__data
 
     def rangeArray(self):
         """迭代打印数组"""
         print "[",
-        for i,r in enumerate(self.data):
+        for i,r in enumerate(self.__data):
             print r,
-            if i < len(self.data)-1:
+            if i < len(self.__data)-1:
                 print ',',
         print "]"
 
     def filterAr(self,v):
         """判断输入值是否是指定数组元素的约数，生成是或否的新数组"""
         newar = []
-        for a in self.data:
+        for a in self.__data:
             if a % v == 0:
                 newar.append(str(v) + '是')
             else:
@@ -36,8 +44,8 @@ class array:
     def maximum(self):
         """遍历数组并查找数组中最大值"""
         max = 0
-        for a in range(1,len(self.data)):
-            if self.data[a] > self.data[max]:
+        for a in range(1,len(self.__data)):
+            if self.__data[a] > self.__data[max]:
                 max = a
         return max
 
@@ -49,32 +57,32 @@ class array:
 
     def selectSort(self):
         """通过选择排序算法实现顺序排列"""
-        for a in range(len(self.data)-1):
-            for x in range(a+1,len(self.data)):
-                if self.data[a] > self.data[x]:
-                    self.__swap(self.data,a,x)
+        for a in range(len(self.__data)-1):
+            for x in range(a+1,len(self.__data)):
+                if self.__data[a] > self.__data[x]:
+                    self.__swap(self.__data,a,x)
 
     def bubbleSort(self):
         """通过冒泡排序算法实现顺序排列"""
-        for a in range(1,len(self.data)):
-            for x in range(len(self.data)-a):
-                if self.data[x] > self.data[x+1]:
-                    self.__swap(self.data,x,x+1)
+        for a in range(1,len(self.__data)):
+            for x in range(len(self.__data)-a):
+                if self.__data[x] > self.__data[x+1]:
+                    self.__swap(self.__data,x,x+1)
 
     def bubbleSortPro(self):
         """通过冒泡排序算法实现顺序排列,利用中间变量提高性能"""
-        for a in range(len(self.data)-1):
-            tmp = self.data[0]
+        for a in range(len(self.__data)-1):
+            tmp = self.__data[0]
             tp = 0
-            for x in range(1,len(self.data)-a):
-                if self.data[x] > tmp:
-                    tmp = self.data[x]
+            for x in range(1,len(self.__data)-a):
+                if self.__data[x] > tmp:
+                    tmp = self.__data[x]
                     tp = x
-            self.__swap(self.data,tp,x)
+            self.__swap(self.__data,tp,x)
 
     def search(self,v):
         """在指定数组中循环查找给定值,并返回值所在角标"""
-        for i,a in enumerate(self.data):
+        for i,a in enumerate(self.__data):
             if v == a:
                 return i
         else:
@@ -82,14 +90,14 @@ class array:
 
     def binarySearch(self,v):
         """在指定数组中通过折半查找算法查找给定值,并返回值所在角标"""
-        bubbleSortPro(self.data)
+        bubbleSortPro(self.__data)
         low = 0
-        high = len(self.data)-1
+        high = len(self.__data)-1
         while low <= high:
             mid = (low + high)/2
-            if self.data[mid] == v:
+            if self.__data[mid] == v:
                 return mid
-            elif self.data[mid] > v:
+            elif self.__data[mid] > v:
                high = mid - 1
             else:
                low = mid + 1
@@ -105,3 +113,4 @@ if __name__ == '__main__':
     a.rangeArray()
     a.bubbleSort()
     a.rangeArray()
+    print a.getData()
