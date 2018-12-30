467479885121bc111bc302909e8ec62bc631822b
fnfly2005 <fnfly2005@aliyun.com>
2018-12-30 22:40:53 +0800

    网页文档自动生成-java

--- learntoprogram/array.java
+++ learntoprogram/array.java
@@ -1,32 +1,51 @@
-/*数组&类*/
-class Array//类名首字母大写
+/**
+* Description: 
+* 数组和类
+* @author fnfly2005
+* @version 1.0
+*/
+public class Array//类名首字母大写
 {
     private int[] data;//封装实例变量
     static int dt = 5;//类变量，各实例对象共享，直接被类所引用-Array.dt,同样的概念有类函数、类代码块
     
+    //构造函数，初始化实例变量,首字母大写
     Array(int[] arr)
-    {//构造函数，初始化实例变量,首字母大写
+    {
         this.data = arr;
     }
 
+    //构造函数重载
     Array(int[] arr,String inf)
-    {//构造函数重载
+    {
         this(arr);
         System.out.println(inf);
     }
 
+    /**
+    * 访问内部数据的对外方法
+    * @return 数组对象的值
+    */
     public int[] getData()
-    {//访问内部数据的对外方法
+    {
         return this.data;
     }
 
+    /**
+    * 改变内部数据的对外方法
+    * @param arr 赋值给数组对象
+    */
     public void setData(int[] arr)
-    {//改变内部数据的对外方法
+    {
         this.data = arr;
     }
-
-    int maximum()
-    {//遍历数组并查找数组中最大值的角标
+    
+    /**
+    * 遍历数组并查找数组中最大值
+    * @return 最大值的角标
+    */
+    public int maximum()
+    {
         int max = 0;
         for(int i=1;i<this.data.length;i++)
         {
@@ -39,14 +58,14 @@ class Array//类名首字母大写
     }
 
     private void swap(int[] array,int a1,int a2)
-    {//实现数组中两个值的位置互换
+    {/**实现数组中两个值的位置互换*/
         int tmp = array[a1];
         array[a1] = array[a2];
         array[a2] = tmp;
     }
 
-    void selectSort()
-    {//通过选择排序算法实现顺序排列
+    public void selectSort()
+    {/**通过选择排序算法实现顺序排列*/
         for(int x=0;x<this.data.length-1;x++)
         {
             for(int y=x+1;y<this.data.length;y++)
@@ -59,8 +78,8 @@ class Array//类名首字母大写
         }
     }
 
-    void bubbleSort()
-    {//通过冒泡排序算法实现顺序排列
+    public void bubbleSort()
+    {/**通过冒泡排序算法实现顺序排列*/
         for (int x=1;x<this.data.length-1;x++)
         {
             for (int y=0;y<this.data.length-x;y++)
@@ -73,8 +92,8 @@ class Array//类名首字母大写
         }
     }
 
-    void bubbleSortPro()
-    {//通过冒泡排序算法实现顺序排列,利用中间变量提高性能
+    public void bubbleSortPro()
+    {/**通过冒泡排序算法实现顺序排列,利用中间变量提高性能*/
         for (int x=1;x<this.data.length;x++)
         {
             int tmp = 0;
@@ -96,8 +115,8 @@ class Array//类名首字母大写
         }
     }
 
-    int search(int v)
-    {//在指定数组中循环查找给定值,并返回值所在角标
+    public int search(int v)
+    {/**在指定数组中循环查找给定值,并返回值所在角标*/
         for (int x=0;x<this.data.length;x++)
         {
             if(this.data[x] == v)
@@ -108,8 +127,8 @@ class Array//类名首字母大写
         return -1;
     }
 
-    int binarySearch(int v)
-    {//在指定数组中通过折半查找算法查找给定值,并返回值所在角标
+    public int binarySearch(int v)
+    {/**在指定数组中通过折半查找算法查找给定值,并返回值所在角标*/
         int min = 0,max = this.data.length-1,mid;
         while (min <= max)
         {    
@@ -130,8 +149,8 @@ class Array//类名首字母大写
         return -1;
     }
 
-    void loopArray()
-    {//遍历并打印数组
+    public void loopArray()
+    {/**遍历并打印数组*/
         System.out.print('[');
         for(int a=0;a<this.data.length;a++)
         {
