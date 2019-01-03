1f99b67a1c2b48e4519882f51b94876d90e13ca0
fnfly2005 <fnfly2005@aliyun.com>
2019-01-03 14:38:01 +0800

    单例模式之懒汉式-java

--- learntoprogram/Single.java
+++ learntoprogram/Single.java
@@ -4,26 +4,54 @@
 * @version 1.0
 * @author fnfly2005
 */
-class Single
+//饿汉式
+class SingleEager
 {
-    //饿汉式
-    private static Single s = new Single();//创建本类私有静态对象
+    private static SingleEager s = new SingleEager();//创建本类私有静态对象
 
-    private Single()
+    private SingleEager()
     {
     //私有化构造函数
     }
 
-    public static Single getInstance()
+    public static SingleEager getInstance()
     {
     //开放一个对外方法把返回对象
        return s; 
     }
+}
 
+//懒汉式
+class SingleLazy
+{
+    private static SingleLazy s = null;//创建本类私有静态对象
+
+    private SingleLazy()
+    {
+    //私有化构造函数
+    }
+
+    public static SingleLazy getInstance()
+    {
+    //开放一个对外方法把返回对象
+        if (s == null)
+        {
+        //当s为空时新建对象，延迟加载，线程不安全
+            SingleLazy s = new SingleLazy();
+        }
+        return s; 
+    }
+
+}
+
+class Single
+{
     public static void main(String[] args)
     {
-        Single s1 = Single.getInstance();
-        Single s2 = Single.getInstance();
+        //SingleEager s1 = SingleEager.getInstance();
+        //SingleEager s2 = SingleEager.getInstance();
+        SingleLazy s1 = SingleLazy.getInstance();
+        SingleLazy s2 = SingleLazy.getInstance();
         
         System.out.println(s1 == s2);
     }
