b1823b9d571483dfa77dab645cd074a3752d1dc9
fnfly2005 <fnfly2005@aliyun.com>
2019-01-05 23:43:15 +0800

    单例模式之new-python

--- learntoprogram/Single.py
+++ learntoprogram/Single.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python
 #coding:utf-8
 '''
-Description:单例模式之懒汉式-线程不安全
+Description:单例模式
 Version: v1.0
 '''
 
-class Single:
-    '''单例模式示例'''
-    def __init__(self):
-        pass
-    
-    '''开放一个对外方法并返回对象'''
-    @classmethod #表示该方法无需实例化
-    def getInstance(cls):
-        if not hasattr(Single,'ins'):#用于判断对象是否包含对应的属性 
-            Single.ins=Single()
-        return Single.ins
+'''单例模式-线程不安全,继承object成为新式类,按广度优先原则搜索方法和变量
+-区别于无需继承的经典类,按深度优先原则搜索方法和变量'''
+class SingleLazy(object):
+    __ins = None
+
+    '''new方法是创建类实例的方法，需要从object里继承
+    不能使用__init__ 因为__init__在实例生成后使用'''
+    def __new__(cls):
+        if cls.__ins is None:
+            cls.__ins=super(SingleLazy,cls).__new__(cls)
+        return cls.__ins
 
 if __name__== '__main__':
-    s1 = Single.getInstance()
-    s2 = Single.getInstance()
+    s1 = SingleLazy()
+    s2 = SingleLazy()
 
     print s1
     print s2
