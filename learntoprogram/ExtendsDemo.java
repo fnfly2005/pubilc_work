ad19c404a6972a950619894f870dc06099216452
fnfly2005 <fnfly2005@aliyun.com>
2019-01-07 14:28:50 +0800

    子父类方法的覆盖和扩展

--- learntoprogram/ExtendsDemo.java
+++ learntoprogram/ExtendsDemo.java
@@ -8,23 +8,34 @@ class Persion
 {
     int age = 0;
     String name = "fnfly2005";
+
+    void show()
+    {
+        System.out.println(this.age+"..fu.."+this.age);
+    }
 }
 
 class Student extends Persion
 {
     int age = 10;
+
+    /**
+        this代表一个本类对象的引用，super代表父类空间
+        覆盖父类方法,子类权限大于等于父类,静态只能覆盖静态
+    */
     void show()
     {
-        //this代表一个本类对象的引用，super代表父类空间
-        System.out.println(this.age+"...."+super.age);
+        super.show();//保留原有功能
+        System.out.println(this.age+"..zi.."+this.age);//新增扩展功能
     }
+
     void study()
     {
         System.out.println(name+"....student study");
     }
 }
 
-class Work extends Persion
+class Worker extends Persion
 {
     void work()
     {
@@ -37,7 +48,9 @@ class ExtendsDemo
     public static void main(String[] args)
     {
         Student s = new Student();
+        Worker w = new Worker();
         s.show();
+        w.show();
         s.study();
     }
 }
