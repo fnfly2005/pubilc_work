22ca63eb032538dad02dd203063de7da584f74e6
fnfly2005 <fnfly2005@aliyun.com>
2019-01-27 23:30:26 +0800

    逻辑回归demo

--- learnsklearn/linearRegression.py
+++ learnsklearn/linearRegression.py
@@ -34,6 +34,17 @@ def linearMethod(train_data,train_target):
     print "predict: ",linear.predict(X_test)#分类器应用
     print y_test
 
+#逻辑回归函数
+def logisticMethod(train_data,train_target):
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(train_data,train_target,test_size=0.1)#切分数据集
+    linear = linear_model.LogisticRegression(random_state=0)
+    linear.fit(X_train,y_train)
+    print "linear's score: ",linear.score(X_train,y_train)#评分函数，返回R方，拟合优度
+    print "predict: ",linear.predict(X_test)#分类器应用
+    print y_test
+
 if __name__ == '__main__':
-    boston = datasets.load_boston()#导入数据集,波士顿房价数据
-    linearMethod(boston.data,boston.target)#获取特征向量,获得样本标签,应用至模型
+    #boston = datasets.load_boston()#导入数据集,波士顿房价数据
+    #linearMethod(boston.data,boston.target)#获取特征向量,获得样本标签,应用至模型
+    iris = datasets.load_iris()
+    logisticMethod(iris.data,iris.target)
