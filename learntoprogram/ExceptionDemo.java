0585393066e7b8ae813bff8768a39d96ed39bbe1
fnfly2005 <fnfly2005@aliyun.com>
2019-01-28 18:56:05 +0800

    注释-异常说明

--- learntoprogram/ExceptionDemo.java
+++ learntoprogram/ExceptionDemo.java
@@ -2,6 +2,7 @@
 * Description:
 * 异常处理技术,异常对象,自定义异常对象,自定义异常的声明,异常捕捉处理,try..finally
 * 异常与错误的区别：错误error由JVM抛出,一般是严重性错误,无法进行处理,不应该捕获
+* 异常处理的原则，发现异常才进行处理，未发现不处理
 * 异常分两种：编译时异常、运行时异常
 * 编译时异常，希望在编译时进行检测
 * 运行时异常，一般不可处理，无需声明，直接抛出，与错误的区别:运行时异常是JVM正常时抛出的、错误是JVM不正常时抛出的
@@ -35,7 +36,7 @@ class ExceptionDemo
         }
     }
 
-    public static int arrv(int[] load,int index)// throws FuIndexException 继承Exception需要声明
+    public static int arrv(int[] load,int index)// throws FuIndexException,NullPointerException 继承Exception需要声明,可以抛出或捕捉多个异常
     {
         if (load == null)
         {//一般用于类或方法
