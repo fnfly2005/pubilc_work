c0eeb58714dbd81862565d49f5e5b197ec5d10d3
fnfly2005 <fnfly2005@aliyun.com>
2019-01-29 16:50:24 +0800

    异常转换封装

--- learntoprogram/ExceptionDemo.java
+++ learntoprogram/ExceptionDemo.java
@@ -1,6 +1,6 @@
 /**
 * Description:
-* 异常处理技术,异常对象,自定义异常对象,自定义异常的声明,异常捕捉处理,try..finally
+* 异常处理技术,异常对象,自定义异常对象,自定义异常的声明,异常捕捉处理,多异常捕捉,try..finally,异常转换封装
 * 异常与错误的区别：错误error由JVM抛出,一般是严重性错误,无法进行处理,不应该捕获
 * 异常处理的原则，发现异常才进行处理，未发现不处理
 * 异常分两种：编译时异常、运行时异常
@@ -11,8 +11,18 @@
 */
 class ExceptionDemo
 {
-    public static void main(String[] args)// throws FuIndexException 继承Exception需要声明
+    public static void main(String[] args) throws MaoYanException,NoPlanException// 继承Exception需要声明或抛出
     {
+        Teacher t = new Teacher("fnfly2005");
+        try
+        {
+            t.prelect();
+        }
+        catch(NoPlanException n)
+        {
+            System.out.println(n.toString());
+            System.out.println("change..people");
+        }
         int[] arr = new int[2];
         int[] load = {1,2};
         try
@@ -36,7 +46,7 @@ class ExceptionDemo
         }
     }
 
-    public static int arrv(int[] load,int index)// throws FuIndexException,NullPointerException 继承Exception需要声明,可以抛出或捕捉多个异常
+    public static int arrv(int[] load,int index)
     {
         if (load == null)
         {//一般用于类或方法
@@ -54,6 +64,92 @@ class ExceptionDemo
         
         return load[index];
     }   
+}
+
+class Teacher
+{
+    private String name;
+    private Computer comp;
+
+    Teacher(String name)
+    {
+        this.name = name;
+        comp = new Computer();
+    }
+
+    public void prelect() throws MaoYanException,NoPlanException
+    {
+        try
+        {
+            comp.run();
+            System.out.println(name + "...teach");
+        }
+        catch (LanPingException e)
+        {
+            System.out.println(e.toString());
+            comp.reset();
+            prelect();
+        }
+        catch (MaoYanException m)//多异常捕捉
+        {
+            System.out.println(m.toString());
+            test();
+            throw new NoPlanException("plan can't finish");//异常转换
+        }
+    }
+
+    public void test()
+    {
+        System.out.println("allpeople...exercise");
+    }
+}
+
+class Computer 
+{
+    private int state = 2;
+    
+    public void run() throws LanPingException,MaoYanException
+    {
+        if (state == 1)
+        {
+            throw new LanPingException("computer..lanping!!");
+        }
+        if (state == 2)
+        {
+            throw new MaoYanException("computer..maoyan!!");
+        }
+        System.out.println("computer...run");
+    }
+
+    public void reset()
+    {
+        state = 0;
+        System.out.println("computer...reset");
+    }
+}
+
+class LanPingException extends Exception
+{
+    LanPingException(String msg)
+    {
+        super(msg);
+    }
+}
+
+class MaoYanException extends Exception
+{
+    MaoYanException(String msg)
+    {
+        super(msg);
+    }
+}
+
+class NoPlanException extends Exception
+{
+    NoPlanException(String msg)
+    {
+        super(msg);
+    }
 }
 
 /*
