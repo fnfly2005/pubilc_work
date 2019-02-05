8728ebb7ceab5a91d4ec5e5b9bcc48fa75dfa297
fnfly2005 <fnfly2005@aliyun.com>
2019-02-05 23:36:13 +0800

    去除冗余代码-优化

--- learnsklearn/linearRegression.py
+++ learnsklearn/linearRegression.py
@@ -1,65 +1,38 @@
 #!/usr/bin/python
 # coding: utf-8
-##################################
 """
 Description: 回归算法
-Version: v1.0
+最小二乘法、岭回归、套索回归、弹性网络、多项式
 """
-##################################
-from sklearn import datasets #数据集模块
-from sklearn import preprocessing #特征处理模块
-from sklearn import linear_model #广义线性模块
-from sklearn import model_selection #模型选择模块
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+from sklearn import datasets
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model as lm
+from sklearn.model_selection import train_test_split
 
-path = '/Users/fannian/Documents/'
-#支持外部导入数据，返回的数列可直接应用train_test_split函数
-def loadData(datefile):
-    df = pd.read_csv(datefile,sep='\t')# 读取数据
-    df['日期']=pd.to_datetime(df['日期'],format='%Y-%m-%d')
-    return df[["日期","订单量"]].sort_values(by='日期')
+def linearMethod(train_data,train_target,model):
+    X_train, X_test, y_train, y_test = train_test_split(train_data,train_target,test_size=0.2)#切分数据集
 
-#返回等差数组
-def arrayline(array):
-    return np.linspace(1,len(array),num=len(array)
-
-#输入特征和特征标签，输出线性分类器评估值
-def linearMethod(train_data,train_target):
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(train_data,train_target,test_size=0.1)#切分数据集
-
-    #reg = linear_model.ElasticNetCV(normalize=True)#弹性网络回归交叉验证，对惩罚系数进行调优
-    reg = linear_model.RidgeCV(normalize=True)#岭回归交叉验证，对惩罚系数进行调优
-    #reg = linear_model.LassoCV(alphas = [0.001,0.01,0.1,1.0],normalize=True)#套索回归交叉验证，对惩罚系数进行调优
-    reg.fit(train_data,train_target)#训练调优器
-    print "reg.alpha: ",reg.alpha_ #获取最佳惩罚系数
-    
-    #linear = linear_model.ElasticNet(alpha = reg.alpha_,normalize=True)#弹性网络回归，设置normalize参数对特征进行缩放，设置alpha惩罚系数
-    linear = linear_model.Ridge(alpha = reg.alpha_,normalize=True)#岭回归，设置normalize参数对特征进行缩放，设置alpha惩罚系数
-    #linear = linear_model.Lasso(alpha = reg.alpha_,normalize=True)#套索回归，设置normalize参数对特征进行缩放，设置alpha惩罚系数
-    #linear=linear_model.LinearRegression(normalize=True)#最小二乘法，设置normalize参数对特征进行缩放
+    if model == 'Ridge':
+        linear = lm.RidgeCV(normalize=True)#岭回归交叉验证，对惩罚系数进行调优
+    elif model == 'Lasso':
+        linear = lm.LassoCV(normalize=True)#套索回归交叉验证，对惩罚系数进行调优
+        #linear = lm.Lasso(alpha = reg.alpha_,normalize=True)#套索回归，设置normalize参数对特征进行缩放，设置alpha惩罚系数
+    elif model == 'ElasticNet':
+        linear = lm.ElasticNetCV(normalize=True)#弹性网络回归交叉验证，对惩罚系数进行调优
+        #linear = lm.ElasticNet(alpha = reg.alpha_,normalize=True)#弹性网络回归，设置normalize参数对特征进行缩放，设置alpha惩罚系数
+    else:
+        linear = lm.LinearRegression(normalize=True)#最小二乘法，设置normalize参数对特征进行缩放
 
     linear.fit(X_train,y_train)#训练模型参数
-
-    py_test=linear.predict(X_test)
-    print "linear's score: ",linear.score(X_train,y_train)#评分函数，返回R方，拟合优度
-
-    #测试集与预测数据绘图比较
-    fig = plt.figure()
-    fig.suptitle('no asxes on this figure')
-    fig,ax_lst = plt.subplots(1,1) #图形矩阵数量
-    ax_lst.plot(arrayline(y_test),y_test,label="sample")
-    ax_lst.plot(arrayline(py_test),py_test,label="predict")
-    fig.savefig(path + 'figure.png')#输出图片
-
+    #评分函数，返回R方，拟合优度
+    print "train's score: ",linear.score(X_train,y_train)
+    print "test's score: ",linear.score(X_test,y_test)
 
 #特征多项式化处理
 def polynomialMethod(trans_data,deg):
-    poly = preprocessing.PolynomialFeatures(degree = deg)#最高次方
+    poly = PolynomialFeatures(degree = deg)#最高次方
     return poly.fit_transform(trans_data)#转化特征
 
 if __name__ == '__main__':
     boston = datasets.load_boston()#导入数据集,波士顿房价数据
-    #linearMethod(boston.data,boston.target)#获取特征向量,获得样本标签,应用至模型
-    linearMethod(polynomialMethod(boston.data,2),boston.target)
+    linearMethod(polynomialMethod(boston.data,3),boston.target,'Ridge')#获取特征向量,获得样本标签,应用至模型
