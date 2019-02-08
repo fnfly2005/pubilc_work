925d593b29d35db49d6064b4caeb3e305d715157
fnfly2005 <fnfly2005@aliyun.com>
2019-02-08 11:45:50 +0800

    jar包及导入包-java

--- learntoprogram/PackageDemo.java
+++ learntoprogram/PackageDemo.java
@@ -4,6 +4,7 @@
 * @author fnfly2005
 */
 package mypack.subpack;
+import mypack.PackageDemoA;
 class PackageDemo
 {
     public static void main(String[] args)
@@ -11,6 +12,8 @@ class PackageDemo
         mypack.subpack.PackageDemoB b = new mypack.subpack.PackageDemoB();
         b.show();
         b.run();//该run方法为PackageDemoB覆盖的run
+        PackageDemoA a = new PackageDemoA();
+        a.show();
     }
 }
 
