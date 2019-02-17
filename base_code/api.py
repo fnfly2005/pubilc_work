b56fd0ff56adde0f6b46a8aff79bef96c7182f55
fnfly2005 <fnfly2005@aliyun.com>
2019-02-17 21:44:59 +0800

    优化py模板

--- base_code/api.py
+++ base_code/api.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
 #coding:utf-8
-##################################
 """
 Description: 
-Version: v1.0
 """
-##################################
+
 if __name__ == '__main__':
     pass
