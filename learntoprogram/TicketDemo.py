12834480d5987c3c27d70bce586de36061e1d1ad
fnfly2005 <fnfly2005@aliyun.com>
2019-02-25 15:20:37 +0800

    同步锁-python

--- learntoprogram/TicketDemo.py
+++ learntoprogram/TicketDemo.py
@@ -2,6 +2,7 @@
 #coding:utf-8
 """
 Description: 多线程技术-线程数据共享及线程安全问题
+同步锁-基本锁
 """
 import time
 import threading
@@ -9,13 +10,16 @@ import threading
 class Ticket(object):
     def __init__(self,num):
         self.num = num
+        self.r = threading.Lock()
     
     def show(self):
         while True:
+            self.r.acquire()
             if self.num > 0:
-                time.sleep(2)
+                time.sleep(0.002)
                 print threading.currentThread().name + "..sale.." + str(self.num)
             self.num -= 1
+            self.r.release()
 
 class TicketDemo(threading.Thread):
     def __init__(self,ticket):
