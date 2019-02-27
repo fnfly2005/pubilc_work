e3661e0bdc52c78810dbf52d738f481337d0c52f
fnfly2005 <fnfly2005@aliyun.com>
2019-02-27 18:36:59 +0800

    单例模式之装饰器

--- learntoprogram/Single.py
+++ learntoprogram/Single.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python
 #coding:utf-8
 '''
-Description:单例模式
+Description:单例模式、装饰器
 Version: v1.0
 '''
 
-'''单例模式-线程不安全,继承object成为新式类,按广度优先原则搜索方法和变量
--区别于无需继承的经典类,按深度优先原则搜索方法和变量'''
+def singleton(cls):
+    instances = {}
+    def getInstance(*args,**kw):
+        if cls not in instances:
+            instances[cls] = cls(*args,**kw)
+        return instances[cls]
+    return getInstance
+    
+@singleton
 class Single(object):
-    __ins = None
-
-    '''通过new方法返回一个类实例-类，需要从object里继承,不生成实例
-    不能使用__init__ 因为__init__在实例生成后使用'''
-    def __new__(cls):
-        if cls.__ins is None:
-            cls.__ins=object.__new__(cls)
-        return cls.__ins
+    x = 100
+    def __init__(self):
+        pass
 
 if __name__== '__main__':
-    s1 = Single()
-    s2 = Single()
-
-    print s1
-    print s2
+    print Single()
+    print Single()
