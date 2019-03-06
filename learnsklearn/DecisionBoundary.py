ac2c52cc0ff4178954ae291a5db5b8a13c37560a
fnfly2005 <fnfly2005@aliyun.com>
2019-03-06 16:47:08 +0800

    优化代码-新增逻辑回归决策边界

--- learnsklearn/DecisionBoundary.py
+++ learnsklearn/DecisionBoundary.py
@@ -1,75 +1,69 @@
 #!/usr/bin/python
 #coding:utf-8
 """
-Description: 感知器算法-分类类库调用版
+Description: 决策边界可视化
 """
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Perceptron
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 plt.switch_backend('agg')
 
-iris = datasets.load_iris()
-X = iris.data[:,[2,3]]
-y = iris.target
+class fitScore(object):
+    '''模型训练'''
+    def __init__(self,X,y,model):
+        X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3)
 
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3)
+        sc = StandardScaler()
+        sc.fit(X_train)
+        X_train_std = sc.transform(X_train)
+        X_test_std = sc.transform(X_test)
 
-sc = StandardScaler()
-sc.fit(X_train)
-X_train_std = sc.transform(X_train)
-X_test_std = sc.transform(X_test)
+        self.X_combined_std = np.vstack((X_train_std,X_test_std))
+        self.y_combined = np.hstack((y_train,y_test))
+        self.model = model
+        self.model.fit(X_train_std,y_train)
 
-ppn = Perceptron(max_iter=40, eta0=0.001)
-ppn.fit(X_train_std,y_train)
-y_pred = ppn.predict(X_test_std)
+        y_pred = self.model.predict(X_test_std)
+        print (y_test != y_pred).sum() #返回错误的样本数
+        print '%.2f' % accuracy_score(y_test,y_pred) #分类准确率
 
 def plot_decision_regions(X,y,classifier,test_idx=None,resolution=0.02):
-    '''绘出决策界限'''
-    #设置颜色和标记
-    markers = ('s','x','o','^','v')
-    colors = ('red','blue','lightgreen','gray','cyan')
+    '''绘出决策边界'''
+    markers = ('s','x','o','^','v')#设置标记
+    colors = ('red','blue','lightgreen','gray','cyan')#设置颜色
     cmap = ListedColormap(colors[:len(np.unique(y))])
-
     #两个特征的值域范围
     x1_min, x1_max = X[:,0].min() - 1, X[:,0].max() + 1
     x2_min, x2_max = X[:,1].min() - 1, X[:,1].max() + 1
     #从坐标向量中返回坐标矩阵
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),\
-                        np.arange(x2_min, x2_max, resolution))
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),np.arange(x2_min, x2_max, resolution))
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
-    #绘制等高线
-    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap = cmap) 
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap = cmap) #绘制等高线
     #设置横纵坐标轴范围
     plt.xlim(xx1.min(),xx1.max())
     plt.ylim(xx2.min(),xx2.max())
-
     #画出所有样本点
     X_test, y_test = X[test_idx, :],y[test_idx]
     for idx,cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0], y=X[y == cl,1],\
-                alpha=0.8, c=cmap(idx),\
-                marker=markers[idx], label=cl)
-    #高亮所有样本点
-    if test_idx:
-        X_test, y_test =X[test_idx,:],y[test_idx]
-        plt.scatter(X_test[:,0],X_test[:,1], c='',\
-            alpha=1.0, linewidth=1,marker='o',\
-            s=55,label='test set')
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl,1],alpha=0.8, c=cmap(idx),marker=markers[idx], label=cl)
 
 if __name__ == '__main__':
-    path = '/Users/fannian/Documents/'
-    print np.unique(y) #返回去重后的y矩阵
-    print (y_test != y_pred).sum() #返回错误的样本数
-    print '%.2f' % accuracy_score(y_test,y_pred) #分类准确率
-    X_combined_std = np.vstack((X_train_std,X_test_std))
-    y_combined = np.hstack((y_train,y_test))
-    plot_decision_regions(X=X_combined_std,y=y_combined,classifier=ppn,test_idx=range(105,150))
+    path = '/Users/fannian/Downloads/'
+    iris = datasets.load_iris()
+    X = iris.data[:,[2,3]]
+    y = iris.target
+    ppn = Perceptron(max_iter=40, eta0=0.001)
+    lr = LogisticRegression(C=1000)
+    f = fitScore(X=X,y=y,model=lr)
+
+    plot_decision_regions(X=f.X_combined_std,y=f.y_combined,classifier=f.model,test_idx=range(105,150))
     plt.xlabel('petal length [standardized]')
     plt.ylabel('petal width [standardized]')
     plt.legend(loc='upper left') #显示图例
