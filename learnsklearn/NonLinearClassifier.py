e09f523649bc76411a471b9eb0d1d85dc6e58fe0
fnfly2005 <fnfly2005@aliyun.com>
2019-03-10 15:58:59 +0800

    优化注释

--- learnsklearn/NonLinearClassifier.py
+++ learnsklearn/NonLinearClassifier.py
@@ -2,6 +2,7 @@
 #coding:utf-8
 """
 Description: 非线性分类器
+#svm-基于高斯核函数的SVM，对线性不可分的数据集有效
 """
 from sklearn import svm
 import DecisionBoundary as db
@@ -15,13 +16,16 @@ class XorSample(object):
         y_xor = np.logical_xor(self.X[:,0] > 0, self.X[:,1] > 0) #计算X_xor两个特征的异或矩阵
         self.y = np.where(y_xor,1,-1)
 
-
 if __name__ == '__main__':
     #path = '/Users/fannian/Downloads/'
     path = '/home/fannian/downloads/'
     s = XorSample(200)
-    svm = svm.SVC(C=1,kernel='rbf',gamma=1)#svm-基于高斯核函数的SVM，对线性不可分的数据集有效
+    
+    '''参数C越大，惩罚影响越小，模型倾向于过拟合
+    参数gamma越大，模型倾向于过拟合'''
+    svm = svm.SVC(C=10,kernel='rbf',gamma=1)
     svm.fit(s.X,s.y)
+    
     db.plot_decision_regions(s.X,s.y,classifier=svm)
     plt.legend(loc='upper left')
     plt.savefig(path + 'decision_figure.png')
