75f1ae3341b25502d087f1c7e2d1f340ad4da1b6
fnfly2005 <fnfly2005@aliyun.com>
2019-03-10 20:10:11 +0800

    随机森林

--- learnsklearn/DecisionTreeDemo.py
+++ learnsklearn/DecisionTreeDemo.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
 #coding:utf-8
 """
-Description: 决策树
+Description: 决策树及其可视化,随机森林
 """
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.tree import export_graphviz
+from sklearn.ensemble import RandomForestClassifier
 import DecisionBoundary as db
 
 if __name__ == '__main__':
     from sklearn import datasets
+    from sklearn.tree import export_graphviz
     #path = '/Users/fannian/Downloads/'
     path = '/home/fannian/downloads/'
     iris = datasets.load_iris()
     tree = DecisionTreeClassifier(criterion='entropy',max_depth=3)
+    forest = RandomForestClassifier(criterion='entropy',n_estimators=10,n_jobs=2)
     f = db.fitScore(X=iris.data[:,[2,3]],y=iris.target,model=tree)
     f.getScore()
     db.plot_decision_regions(X=f.X_combined_std,y=f.y_combined,classifier=f.linear,test_idx=range(105,150),path_file = path + 'decision_figure.png')
