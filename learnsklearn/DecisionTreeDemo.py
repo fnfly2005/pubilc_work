a89664862cc07f51f18fa109bdce7478d29fd93c
fnfly2005 <fnfly2005@aliyun.com>
2019-03-16 22:00:57 +0800

    适配网格搜索调参&决策树可视化BUG修复

--- learnsklearn/DecisionTreeDemo.py
+++ learnsklearn/DecisionTreeDemo.py
@@ -13,9 +13,21 @@ if __name__ == '__main__':
     #path = '/Users/fannian/Downloads/'
     path = '/home/fannian/downloads/'
     iris = datasets.load_iris()
-    tree = DecisionTreeClassifier(criterion='entropy',max_depth=3)
-    forest = RandomForestClassifier(criterion='entropy',n_estimators=10,n_jobs=2)
-    f = db.fitScore(X=iris.data[:,[2,3]],y=iris.target,model=tree)
+    X = iris.data[:,[2,3]]
+    y = iris.target
+
+    param_grid = [{'clf__max_depth':[2,3,4]}]
+    tree = DecisionTreeClassifier()
+
+    forest = RandomForestClassifier(n_estimators=10,n_jobs=2)
+
+    f = db.fitScore(X=X,y=y,model=tree,param_grid=param_grid)
     f.getScore()
-    db.plot_decision_regions(X=f.X_combined_std,y=f.y_combined,classifier=f.linear,test_idx=range(105,150),path_file = path + 'decision_figure.png')
-    export_graphviz(tree,out_file=path +'tree.dot',feature_names=['petal length','petal width'])
+    db.plot_decision_regions(X=f.X_combined_std,y=f.y_combined,classifier=f.linear,\
+        test_idx=range(105,150),path_file = path + 'decision_figure.png')
+
+    #export_graphviz 会检测模型是否为决策树，如果为其他模型，则会报错，固使用网格搜索需要重新训练
+    tre = DecisionTreeClassifier(max_depth=f.linear.best_params_['clf__max_depth'])
+    tre.fit(X,y)
+    export_graphviz(tre,out_file=path +'tree.dot',\
+        feature_names=['petal length','petal width'])
