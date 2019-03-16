ef1076e0c74de2752aea7a02d3f03c33acfe9378
fnfly2005 <fnfly2005@aliyun.com>
2019-03-16 22:58:59 +0800

    网格搜索嵌套交叉验证技术-机器学习

--- learnsklearn/LinearClassifier.py
+++ learnsklearn/LinearClassifier.py
@@ -6,6 +6,7 @@ Description: 分类算法、降维、求解器、管道流水线、网格搜索
 降维：PCA 
 求解器：随机梯度下降、平均梯度下降
 """
+import numpy as np
 
 class ClassifyPipeline(object):
     def __init__(self,train_data,train_target,linear,param_grid,n_com=0.99):
@@ -14,18 +15,21 @@ class ClassifyPipeline(object):
         from sklearn.model_selection import train_test_split
         from sklearn.decomposition import PCA
         from sklearn.model_selection import GridSearchCV
+        from sklearn.model_selection import cross_val_score
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(train_data,train_target,test_size=0.2)#切分数据集
         scaler = StandardScaler()#对特征数据进行归一化处理
         pcaer = PCA(n_components = n_com) #PCA压缩数据-保留n_com%的信息或n_com个维度
         pipe_cv = Pipeline([('scl',scaler),('pca',pcaer),('clf',linear)])#流水线整合各个模型,可通过标识符访问其中各个元素
         self.linear = GridSearchCV(estimator = pipe_cv,param_grid = param_grid,\
-            scoring = 'accuracy',cv = 10,n_jobs=-1)#网格搜索实现半自动调参
+            scoring = 'accuracy',cv = 5,n_jobs=-1)#网格搜索实现半自动调参
         self.linear.fit(self.X_train,self.y_train)#训练模型
+        self.scores = cross_val_score(self.linear,train_data,train_target,\
+            scoring='accuracy',cv=5)#嵌套交叉验证比较不同模型之间的性能
 
     def getScore(self):
         print "best_score: ",self.linear.best_score_
         print "best_params: ",self.linear.best_params_
-        print "test's score: ",self.linear.score(self.X_test,self.y_test)
+        print "CV accuracy: %.3f +/- %.3f" % (np.mean(self.scores), np.std(self.scores))
 
 if __name__ == '__main__':
     from sklearn.linear_model import LogisticRegression
