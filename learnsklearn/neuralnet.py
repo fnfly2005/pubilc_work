6d71a89e8f2982953bb9315d571586b6ac815d36
fnfly2005 <fnfly2005@aliyun.com>
2019-07-09 21:13:09 +0800

    学习 梯度检验调试

--- learnsklearn/neuralnet.py
+++ learnsklearn/neuralnet.py
@@ -135,6 +135,19 @@ class NeuralNetMLP(object):
                 #compute gradient via backpropagation
                 grad1,grad2 = self._get_gradient(a1=a1,a2=a2,a3=a3,z2=z2,y_enc=y_enc[:,idx],w1=self.w1,w2=self.w2)
 
+                ## start gradient checking
+                grad_diff = self._gradient_checking(X=X[idx],y_enc=y_enc[:,idx],
+                                                    w1=self.w1,w2=self.w2,epsilon=1e-5,
+                                                    grad1=grad1,grad2=grad2)
+                if grad_diff <= 1e-7:
+                    print ('OK: %s' % grad_diff)
+                elif grad_diff <= 1e-4:
+                    print ('Warning: %s' % grad_diff)
+                else:
+                    print ('PROBLEM: %s' % grad_diff)
+
+                ## end gradient checking
+
                 #update weights
                 delta_w1,delta_w2 = self.eta*grad1,self.eta*grad2
                 self.w1 -= (delta_w1 + (self.alpha * delta_w1_prev))
@@ -142,6 +155,49 @@ class NeuralNetMLP(object):
                 delta_w1_prev, delta_w2_prev = delta_w1, delta_w2
 
         return self
+
+    def _gradient_checking(self,X,y_enc,w1,w2,epsilon,grad1,grad2):
+        """
+        Apply gradient checking (for debugging only)
+        Returns
+        -------
+        relative_error:float
+            Relative error between the numerically
+            approximated gradients and the backpropagated gradients.
+        """
+        num_grad1 = np.zeros(np.shape(w1))
+        epsilon_ary1 = np.zeros(np.shape(w1))
+        for i in range(w1.shape[0]):
+            for j in range(w1.shape[1]):
+                epsilon_ary1[i,j] = epsilon
+                a1,z2,a2,z3,a3 = self._feedforward(X,w1-epsilon_ary1,w2)
+                cost1 = self._get_cost(y_enc,a3,w1-epsilon_ary1,w2)
+                a1,z2,a2,z3,a3 = self._feedforward(X,w1+epsilon_ary1,w2)
+                cost2 = self._get_cost(y_enc,a3,w1+epsilon_ary1,w2)
+                num_grad1[i,j] = (cost2 - cost1)/(2*epsilon)
+                epsilon_ary1[i,j] = 0
+
+        num_grad2 = np.zeros(np.shape(w2))
+        epsilon_ary2 = np.zeros(np.shape(w2))
+
+        for i in range(w2.shape[0]):
+            for j in range(w2.shape[1]):
+                epsilon_ary2[i,j] = epsilon
+                a1,z2,a2,z3,a3 = self._feedforward(X,w1,w2-epsilon_ary2)
+                cost1 = self._get_cost(y_enc,a3,w1,w2-epsilon_ary2)
+                a1,z2,a2,z3,a3 = self._feedforward(X,w1,w2+epsilon_ary2)
+                cost2 = self._get_cost(y_enc,a3,w1,w2+epsilon_ary2)
+                num_grad2[i,j] = (cost2 - cost1)/(2*epsilon)
+                epsilon_ary2[i,j] = 0
+
+        num_grad = np.hstack((num_grad1.flatten(),
+                            num_grad2.flatten()))
+        grad = np.hstack((grad1.flatten(),grad2.flatten()))
+        norm1 = np.linalg.norm(num_grad -grad)
+        norm2 = np.linalg.norm(num_grad)
+        norm3 = np.linalg.norm(num_grad)
+        relative_error = norm1 / (norm2 + norm3)
+        return relative_error
 
 if __name__ == '__main__':
     pass
